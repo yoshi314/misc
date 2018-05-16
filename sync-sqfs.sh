a39daf67f98bdb09e3a62b04910690292becea5f
yoshi314 <yoshi314@gmail.com>
2018-05-16 18:03:18 +0200

    Revert "reworked the sync script, it uses emerge now, and wraps the mounts around it"
    This reverts commit fa44592b8f70bb19f07c4daddc2461e643b0279c.

--- sync-sqfs.sh
+++ sync-sqfs.sh
@@ -6,11 +6,10 @@
 sqfs=/dev/shm/portage
 mkdir -p ${sqfs}
 mkdir -p ${sqfs}-work
-mkdir -p ${sqfs}-ro
 
-tempmount=/data/build/ovfs
 # place where the sqfs files are
 location=/data/build
+tempmount=${location}/ovfs
 
 # /config
 
@@ -37,12 +36,7 @@ howmany=$(find $location -maxdepth 1 -name "portage*.sqfs" -type f -mmin -30 | w
 
 
 tempdir=$(mktemp -d)
-#mount -o loop -t auto $squashfile $tempdir || exit 1
-
-
-
-umount /usr/portage
-mount -o loop -t auto $squashfile ${sqfs}-ro || exit 1
+mount -o loop -t auto $squashfile $tempdir
 
 #overlayfs
 #
@@ -52,18 +46,15 @@ mount -o loop -t auto $squashfile ${sqfs}-ro || exit 1
 # ${tempmount} - final mount dir that's the resulting merged mount
 #
 
-
-mount -t overlay overlay -olowerdir=${sqfs}-ro,upperdir=${sqfs},workdir=${sqfs}-work /usr/portage
+mount -t overlay overlay -olowerdir=${tempdir},upperdir=/dev/shm/portage,workdir=/dev/shm/portage-work ${tempmount}
 
 #
 # rsync that shit
 #
 
-#rsync --stats --times --omit-dir-times --compress --whole-file -avr --delete-during rsync://rsync.pl.gentoo.org/gentoo-portage/ ${tempmount}/
+rsync --stats --times --omit-dir-times --compress --whole-file -avr --delete-during --exclude="*ChangeLog" rsync://rsync.pl.gentoo.org/gentoo-portage/ ${tempmount}/
 
-emerge --sync
-
-#find ${tempmount} -name "ChangeLog" -delete
+find ${tempmount} -name "ChangeLog" -delete
 
 #rsync --stats --times --omit-dir-times --compress --whole-file -avr --delete-during --exclude="*ChangeLog" rsync://rsync.pl.gentoo.org/gentoo-portage/ ${tempmount}/
 
@@ -73,14 +64,14 @@ emerge --sync
 
 date=$(date +%F-%H-%M-%S)
 #mksquashfs ${tempmount} ${location}/portage-${date}.sqfs -comp lz4 -Xhc -progress
-mksquashfs /usr/portage ${location}/portage-${date}.sqfs -comp xz -progress
+mksquashfs ${tempmount} ${location}/portage-${date}.sqfs -comp xz -progress
 
 #
 # clean up that shit
 #
 
 umount ${tempmount} && rm -rf ${sqfs} && rm -rf ${sqfs}-work
-umount ${tempdir} && rm -rf ${tempdir} 
+umount ${tempdir} && rm -rf ${tempdir} || exit 1
 umount /usr/portage || exit 1
 
 rm -rf ${tempmount}
@@ -106,9 +97,6 @@ find ${location} -maxdepth 1 -name "portage*sqfs" -mtime +7 -delete
 
 # this is gentoo specific, remove if needed
 mv /var/cache/eix/portage.eix /var/cache/eix/portage.eix.old
-
-layman -s ALL 
-
 eix-update
 eix-diff /var/cache/eix/portage.eix.old /var/cache/eix/portage.eix
 
